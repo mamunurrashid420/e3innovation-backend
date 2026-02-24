38422eae66b7d9fab28849cc99a291a6df0ee7d2
Mamunur Rashid <mamunurr191@gmail.com>
2026-02-24 22:21:21 +0600

    added silder

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -5,17 +5,36 @@ namespace App\Http\Controllers;
 use App\Models\Slider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 
 class SliderController extends Controller
 {
+    /**
+     * Return sliders with full image URL so frontend can show images.
+     */
+    private function slidersWithImageUrl($query)
+    {
+        return $query->get()->map(function ($slider) {
+            $item = $slider->toArray();
+            if (!empty($item['image']) && !str_starts_with($item['image'], 'http')) {
+                $item['image'] = URL::asset($item['image']);
+            }
+            return $item;
+        });
+    }
+
     public function indexPublic()
     {
-        return response()->json(['data' => Slider::where('is_active', true)->orderBy('order_index')->get()]);
+        $sliders = $this->slidersWithImageUrl(
+            Slider::where('is_active', true)->orderBy('order_index')
+        );
+        return response()->json(['data' => $sliders]);
     }
 
     public function index()
     {
-        return response()->json(['data' => Slider::orderBy('order_index')->get()]);
+        $sliders = $this->slidersWithImageUrl(Slider::orderBy('order_index'));
+        return response()->json(['data' => $sliders]);
     }
 
     public function store(Request $request)
@@ -44,7 +63,11 @@ class SliderController extends Controller
 
     public function show(Slider $slider)
     {
-        return response()->json(['data' => $slider]);
+        $data = $slider->toArray();
+        if (!empty($data['image']) && !str_starts_with($data['image'], 'http')) {
+            $data['image'] = URL::asset($data['image']);
+        }
+        return response()->json(['data' => $data]);
     }
 
     public function update(Request $request, Slider $slider)
